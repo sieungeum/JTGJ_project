ded04c1fdc5fd99e989329bedb109ea3ae43408e
30204 <30204@DESKTOP-CREDK50>
2024-10-15 15:37:42 +0900

    (주형) 게시판

--- JTGJProject/src/main/java/com/jtgz/jtgzproject/board/web/BoardController.java
+++ JTGJProject/src/main/java/com/jtgz/jtgzproject/board/web/BoardController.java
@@ -1,13 +1,28 @@
 package com.jtgz.jtgzproject.board.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jtgz.jtgzproject.board.dto.BoardDTO;
+import com.jtgz.jtgzproject.board.service.BoardService;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	BoardService boardService;
+	
 	@RequestMapping("/boardView")
-	public String boardView() {
+	public String boardView(Model model) {
+
+		List<BoardDTO> boardList = boardService.getBoardList();
+		
+		model.addAttribute("boardList", boardList);
+		
 		return "board/boardView";
 	}
 }
